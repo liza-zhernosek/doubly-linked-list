d98cc6ff0040447affae21e5ba1556400eb06850
Liza Zhernosek <liza.zhernosek@bk.ru>
2017-01-28 00:09:55 +0300

    linked-list/methods: implement base methods

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,58 @@
-const Node = require('./node');
+class Node {
+    constructor(data = null, prev = null, next = null) {
+        this.data = data;
+        this.prev = prev;
+        this.next = next;
+    }
+}
 
 class LinkedList {
-    constructor() {}
+  constructor() {
+    this.length = 0;   //initial length of linked list
+    this._head = null;  //first node of linked list
+    this._tail = null;  //last node of linked list
+  }
 
-    append(data) {}
+  append(data) {
+    this.length++;
+    var newNode = new Node(data, null, null);
+    this._head = newNode;
+    this._tail = newNode;
+  }
 
-    head() {}
+  head() {
+    return this._head.data;
+  }
 
-    tail() {}
+  tail() {
+    return this._tail.data;
+  }
 
-    at(index) {}
+  at(index) {
+    var currentNode = this._head;
+    for (var i = 0; i < index; i++) {   //iterate through linked list
+      currentNode = currentNode.next;
+    }
+    return currentNode.data;
+  }
 
-    insertAt(index, data) {}
+  insertAt(index, data) {}
 
-    isEmpty() {}
+  isEmpty() {
+    if (this.length !== 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 
-    clear() {}
+  clear() {}
 
-    deleteAt(index) {}
+  deleteAt(index) {}
 
-    reverse() {}
+  reverse() {}
 
-    indexOf(data) {}
+  indexOf(data) {}
 }
 
 module.exports = LinkedList;
